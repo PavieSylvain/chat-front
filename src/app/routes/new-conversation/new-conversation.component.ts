67976c2adc3ed447a11db2d50c4ac50215689529
PavieSylvain <pavie.sylvain@sfr.fr>
2022-01-10 11:02:32 +0100

    first commit

---
+++ src/app/routes/new-conversation/new-conversation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-conversation',
+  templateUrl: './new-conversation.component.html',
+  styleUrls: ['./new-conversation.component.css']
+})
+export class NewConversationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
